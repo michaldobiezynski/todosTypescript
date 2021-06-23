50a5f9a0fb05e8da81b54b981f1f88c3a4580807
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-06-23 17:43:30 +0100

    object literal annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,3 +9,19 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+//Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
