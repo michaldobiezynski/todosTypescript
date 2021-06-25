69e7375e2b9aafdbdc05ccf29d9a51ce01ed054b
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-06-25 11:41:26 +0100

    multiple types in arrays

--- arrays.ts
+++ arrays.ts
@@ -2,3 +2,7 @@ const carMakers: string[] = ["ford", "toyota", "chevy"];
 const dates = [new Date(), new Date()];
 
 const carsByMake = [["f150"], ["corolla"], ["camaro"]];
+
+const importantDates: (Date | string)[] = [new Date(), "2030-10-10"];
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
